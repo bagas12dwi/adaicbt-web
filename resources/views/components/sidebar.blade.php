a1b97ca97718d2d532c2a1035de22dcdb79dd1c1
bagas12dwi <bag12dwi.s@gmail.com>
2024-01-25 17:45:47 +0700

    edit asset in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none">
-    <a class="navbar-brand me-lg-5" href="../../index.html">
-        <img class="navbar-brand-dark" src="../../assets/img/brand/light.svg" alt="Volt logo" /> <img
-            class="navbar-brand-light" src="../../assets/img/brand/dark.svg" alt="Volt logo" />
+    <a class="navbar-brand me-lg-5" href="{{ url('#') }}">
+        <img class="navbar-brand-dark" src="{{ URL::asset('/assets/img/brand/light.svg') }}" alt="Volt logo" /> <img
+            class="navbar-brand-light" src="{{ URL::asset('/assets/img/brand/dark.svg') }}" alt="Volt logo" />
     </a>
     <div class="d-flex align-items-center">
         <button class="navbar-toggler d-lg-none collapsed" type="button" data-bs-toggle="collapse"
@@ -32,7 +32,8 @@
             <li class="nav-item">
                 <a href="{{ url('#') }}" class="nav-link d-flex align-items-center">
                     <span class="sidebar-icon">
-                        <img src="../../assets/img/brand/light.svg" height="20" width="20" alt="Volt Logo">
+                        <img src="{{ URL::asset('/assets/img/brand/light.svg') }}" height="20" width="20"
+                            alt="Volt Logo">
                     </span>
                     <span class="mt-1 ms-1 sidebar-text">ADA-ICBT Admin</span>
                 </a>
